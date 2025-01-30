9641409daad32e2bfb6979363546890541fefee3
MrRobob <romansmirnoff@gmx.de>
2025-01-30 12:26:53 +0100

    module für wörter erstellt

---
+++ Hangman/public/words.js
@@ -0,0 +1,11 @@
+const words = ['algorithm', 'encryption', 'interface', 'developer', 'deployment', 'processor', 'mainframe', 'timestamp', 'backslash', 'namespace', 
+    'databreach', 'heuristic', 'iteration', 'hyperlink', 'megabyte', 'bytearray', 'checksum', 'stateless', 'networking', 'histogram', 'executable', 
+    'framework', 'bandwidth', 'plaintext', 'recursion', 'firewalls', 'binarytree', 'loadbalanc', 'database', 'keylogger', 'compilers', 'cloudshare', 
+    'middleware', 'registries', 'formatting', 'scripting', 'debuglevel', 'throughput', 'bootloader', 'datacenter', 'loadfactor', 'containers', 
+    'bytebuffer', 'biosystem', 'subroutine', 'workgroup', 'sessionid', 'fileaccess', 'polymorph', 'scripting', 'checksum', 'plaintext', 'registries', 
+    'workgroup', 'formatting', 'algorithm', 'interface', 'encryption', 'namespace', 'mainframe', 'backslash', 'megabyte', 'heuristic', 'bytearray', 
+    'hyperlink', 'stateless', 'iteration', 'networking', 'deployment', 'framework', 'processor', 'binarytree', 'firewalls', 'recursion', 'developers', 
+    'bootloader', 'loadbalanc', 'database', 'heuristics', 'middleware', 'registries', 'throughput', 'megabytes', 'frameworks', 'timestamps', 
+    'checksum', 'datacenter', 'biosystem', 'scripting', 'subroutine', 'debuglevel', 'bandwidths', 'containers'];
+
+module.exports = words;
--- Hangman/server.js
+++ Hangman/server.js
@@ -9,16 +9,8 @@ const app = express();
 const host = "localhost";
 const port = 8080;
 
-// Array mit zu ratenden Wörtern
-const words = ['algorithm', 'encryption', 'interface', 'developer', 'deployment', 'processor', 'mainframe', 'timestamp', 'backslash', 'namespace', 
-    'databreach', 'heuristic', 'iteration', 'hyperlink', 'megabyte', 'bytearray', 'checksum', 'stateless', 'networking', 'histogram', 'executable', 
-    'framework', 'bandwidth', 'plaintext', 'recursion', 'firewalls', 'binarytree', 'loadbalanc', 'database', 'keylogger', 'compilers', 'cloudshare', 
-    'middleware', 'registries', 'formatting', 'scripting', 'debuglevel', 'throughput', 'bootloader', 'datacenter', 'loadfactor', 'containers', 
-    'bytebuffer', 'biosystem', 'subroutine', 'workgroup', 'sessionid', 'fileaccess', 'polymorph', 'scripting', 'checksum', 'plaintext', 'registries', 
-    'workgroup', 'formatting', 'algorithm', 'interface', 'encryption', 'namespace', 'mainframe', 'backslash', 'megabyte', 'heuristic', 'bytearray', 
-    'hyperlink', 'stateless', 'iteration', 'networking', 'deployment', 'framework', 'processor', 'binarytree', 'firewalls', 'recursion', 'developers', 
-    'bootloader', 'loadbalanc', 'database', 'heuristics', 'middleware', 'registries', 'throughput', 'megabytes', 'frameworks', 'timestamps', 
-    'checksum', 'datacenter', 'biosystem', 'scripting', 'subroutine', 'debuglevel', 'bandwidths', 'containers'];
+// Wörter importieren aus der Datei words.js
+const words = require('./public/words');
 
 // Bereitstellung des Ordners public
 app.use(express.static(path.join(__dirname, 'public')));
